b0016a3e735c76901cf9ec02e2bc7c33f134a3a1
flsysys@163.com <flsysys@163.com>
2016-10-25 17:47:57 +0800

    add code

---
+++ app/src/main/java/com/bameng/ui/account/ChangePswActivity.java
@@ -0,0 +1,13 @@
+package com.bameng.ui.account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChangePswActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_psw);
+    }
+}
